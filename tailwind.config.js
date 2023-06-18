b92551ede2aca784dc854e64316abab7b1e11dcc
Ethan <ysciou@misystex.com>
2023-06-18 21:04:30 +0800

    style: add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
+  daisyui: {
+    themes: ['lemonade'],
+  },
   theme: {
     extend: {},
   },
